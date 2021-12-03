49c1b691fbb651e52e7d5aea02b3adf634a27fa0
LucasHGraciano <lucasgraciano@gec.inatel.br>
2021-12-02 21:35:36 -0300

    Implement Database HotelREservationDB from mysql DB

---
+++ src/main/java/service/Database.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.sql.*;
+
+public class Database {
+
+    Connection connection;
+    Statement statement;
+    ResultSet result;
+    PreparedStatement pst;
+
+    static final String user = "";
+    static final String password = "";
+    static final String database = "HotelREservationDB";
+
+    static final String url = "jdbc:mysql://localhost:3306/" + database + "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+    private boolean check = false;
+
+    public void connect(){
+        try{
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection made sucefully: " + connection);
+        }catch (SQLException e){
+            System.out.println("Connection Error: " e.getMessage());
+        }
+    }
+}
